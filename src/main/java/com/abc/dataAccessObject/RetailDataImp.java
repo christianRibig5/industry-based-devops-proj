fb98f68457924c554946c63d8bc35c871cb0833c
Christian Onyeukwu <christianonyeukwu.com>
2026-05-09 10:22:09 -0400

    updated and fix the warnings

--- src/main/java/com/abc/dataAccessObject/RetailDataImp.java
+++ src/main/java/com/abc/dataAccessObject/RetailDataImp.java
@@ -3,23 +3,20 @@ package com.abc.dataAccessObject;
 import java.util.HashMap;
 import java.util.Map;
 import com.abc.RetailModule;
-import com.abc.dataAccessObject.RetailAccessObject;
-
+// import com.abc.dataAccessObject.RetailAccessObject;
 
 public class RetailDataImp implements RetailAccessObject {
 
-	Map<Integer,RetailModule> users = new HashMap<>();
-	
-	
+	Map<Integer, RetailModule> users = new HashMap<>();
+
 	@Override
 	public void create(RetailModule product) {
-		users.put(product.getProduct_id(),product);
+		users.put(product.getProduct_id(), product);
 	}
 
 	@Override
 	public RetailModule read(int product_id) {
 		return users.get(product_id);
 	}
-	
 
 }
