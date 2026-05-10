78d569894086432986b6bff9f02412759bf8dd89
Christian Onyeukwu <christianonyeukwu.com>
2026-05-10 07:29:48 -0400

    added pods

---
+++ terraform-manifest/create-cluster.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "==============================="
+echo "STEP-1: Create VPC using Terraform"
+echo "==============================="
+cd vpc
+terraform init 
+terraform validate
+terraform plan
+terraform apply -auto-approve
+
+echo
+echo "==============================="
+echo "STEP-2: Create EKS Cluster using Terraform"
+echo "==============================="
+cd ../eks-cluster
+terraform init 
+terraform validate
+terraform plan
+terraform apply -auto-approve
+
+echo
+echo "✅ EKS Cluster and VPC creation completed successfully!"
--- terraform-manifest/eks-cluster/c3-remote-state.tf
+++ terraform-manifest/eks-cluster/c3-remote-state.tf
@@ -1,25 +1,3 @@
-# # Datasource
-# data "terraform_remote_state" "vpc" {
-#   backend = "s3"
-
-#   config = {
-#     bucket = "tfstate-dev-ca-central-1-mr67svo6"
-#     key    = "vpc/dev/terraform.tfstate"
-#     region = var.aws_region
-#   }
-# }
-
-# #output
-# output "vpc_id" {
-#   value = data.terraform_remote_state.vpc.outputs.vpc_id
-# }
-# output "private_subnet_ids" {
-#   value = data.terraform_remote_state.vpc.outputs.private_subnet_ids
-# }
-# output "public_subnet_ids" {
-#   value = data.terraform_remote_state.vpc.outputs.public_subnet_ids
-# }
-
 # --------------------------------------------------------------------
 # Reference the Remote State from VPC Project
 # --------------------------------------------------------------------
